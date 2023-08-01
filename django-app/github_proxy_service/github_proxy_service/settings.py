a5f07473d141c22230ee66bfe9b172478b1fa111
ericplatzek <ericplatzek@gmail.com>
2023-08-01 11:00:50 -0700

    add corsheaders to django

--- django-app/github_proxy_service/github_proxy_service/settings.py
+++ django-app/github_proxy_service/github_proxy_service/settings.py
@@ -36,17 +36,27 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'github_proxy'
+    'github_proxy',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+]
+
+CORS_ALLOW_METHODS = [
+    'GET',
 ]
 
 ROOT_URLCONF = 'github_proxy_service.urls'
